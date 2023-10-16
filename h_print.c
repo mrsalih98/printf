ac795732a107568c4321bd4cfa43d922952852df
AmnaHussien <amnahussin88@gmail.com>
2023-10-16 09:03:31 -0700

    amna

--- h_print.c
+++ h_print.c
@@ -4,19 +4,20 @@
  * h_print - Prints an argument based on its type
  * Description: c programm
  * @fmst: Formatted string
- * @arg: arguments to be printed.
- * @in: ind.
- * @array: array to handle print.
+ * @arg: arguments to be printed
+ * @in: ind
+ * @array: array to handle print
  * @flags: Calculates the flags
  * @width: to get the width
  * @precision: Precision specification
  * @size: Size the specifier
- * Return: 1 or 2;
+ * Return: printed character
  */
 int h_print(const char *fmst, int *in, va_list arg,
 char array[], int flags, int width, int precision, int size)
 {
-int j, unkn_length = 0, printed_ch = -1; fmst_t fmst_arg[] = {
+int j, unkn_length = 0, printed_ch = -1; 
+fmst_t fmst_arg[] = {
 {'c', print_character}, {'s', print_string}, {'%', print_percent},
 {'\0', NULL}
 };
@@ -24,6 +25,7 @@ for (j = 0; fmst_arg[j].fmst != '\0'; j++)
 {
 if (fmst[*in] == fmst_arg[j].fmst)
 return (fmst_arg[j].fm(arg, array, flags, width, precision, size));
+}
 if (fmst_arg[j].fmst == '\0')
 {
 if (fmst[*in] == '\0')
@@ -38,7 +40,6 @@ unkn_length = unkn_length + write(1, " ", 1);
 else if (width)
 {
 --(*in);
-}
 while (fmst[*in] != ' ' && fmst[*in] != '%')
 {
 --(*in);
